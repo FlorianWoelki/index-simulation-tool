9b0bf436cfe5a0b2bdba78a0f1d2b95567736eb6
FlorianWoelki <FlorianWoelki@gmx.de>
2024-05-02 09:42:49 +0200

    refactor(benchmark): move BenchmarkConfig to benchmark dir

--- src/benchmark/mod.rs
+++ src/benchmark/mod.rs
@@ -4,6 +4,66 @@ use crate::{index::Index, query::Query};
 
 mod metrics;
 
+/// Configuration for running benchmark on different dataset configurations.
+///
+/// This structure defines the range and step values for varying parameters
+/// in benchmark tests, specifically focusing on the dimensions of the data
+/// and the number of images used in each test set. It also specifies the
+/// range of values each data point can hold.
+/// Dimensions in a dataset refer to the number of attributes or features
+/// each data point (e.g., image) has.
+/// For example, a 3-dimensional vector might represent an RGB color value.
+pub struct BenchmarkConfig {
+    /// The starting number of dimensions for the test data vectors.
+    start_dimensions: usize,
+    /// The maximum number of dimensions to be tested.
+    end_dimensions: usize,
+    /// The increment in dimensions for each subsequent test after the
+    /// initial start_dimensions.
+    step_dimensions: usize,
+    /// The starting number of data points to use in the dataset for the
+    /// benchmarks.
+    start_num_images: usize,
+    /// The maximum number of data points to be tested.
+    end_num_images: usize,
+    /// The increment in the number of data points from one dataset to the
+    /// next. This helps in assessing scalability and performance as the
+    /// amount of data increases.
+    step_num_images: usize,
+    /// A tuple representing the inclusive minimum and maximum values that
+    /// any single element in the data vectors can take. This is crucial for
+    /// generating test data with realistic variability.
+    value_range: (f64, f64),
+}
+
+impl BenchmarkConfig {
+    pub fn new(
+        dimensions_range: (usize, usize, usize),
+        num_images_range: (usize, usize, usize),
+        value_range: (f64, f64),
+    ) -> Self {
+        BenchmarkConfig {
+            start_dimensions: dimensions_range.0,
+            end_dimensions: dimensions_range.1,
+            step_dimensions: dimensions_range.2,
+            start_num_images: num_images_range.0,
+            end_num_images: num_images_range.1,
+            step_num_images: num_images_range.2,
+            value_range,
+        }
+    }
+
+    pub fn dataset_configurations<'a>(&'a self) -> impl Iterator<Item = (usize, usize)> + 'a {
+        (self.start_dimensions..=self.end_dimensions)
+            .step_by(self.step_dimensions)
+            .flat_map(|dimensions| {
+                (self.start_num_images..=self.end_num_images)
+                    .step_by(self.step_num_images)
+                    .map(move |num_images| (dimensions, num_images))
+            })
+    }
+}
+
 pub struct BenchmarkResult {
     pub total_execution_time: Duration,
     pub index_execution_time: Duration,
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use benchmark::{Benchmark, BenchmarkResult};
+use benchmark::{Benchmark, BenchmarkConfig, BenchmarkResult};
 use data::{generator::DataGenerator, HighDimVector};
 use index::{naive::NaiveIndex, DistanceMetric, Index};
 use query::{naive::NaiveQuery, Query};
@@ -13,66 +13,6 @@ mod query;
 fn main() {
     run_benchmark::<HNSWQuery>();
     run_benchmark::<NaiveQuery>();
-}
-
-/// Configuration for running benchmark on different dataset configurations.
-///
-/// This structure defines the range and step values for varying parameters
-/// in benchmark tests, specifically focusing on the dimensions of the data
-/// and the number of images used in each test set. It also specifies the
-/// range of values each data point can hold.
-/// Dimensions in a dataset refer to the number of attributes or features
-/// each data point (e.g., image) has.
-/// For example, a 3-dimensional vector might represent an RGB color value.
-struct BenchmarkConfig {
-    /// The starting number of dimensions for the test data vectors.
-    start_dimensions: usize,
-    /// The maximum number of dimensions to be tested.
-    end_dimensions: usize,
-    /// The increment in dimensions for each subsequent test after the
-    /// initial start_dimensions.
-    step_dimensions: usize,
-    /// The starting number of data points to use in the dataset for the
-    /// benchmarks.
-    start_num_images: usize,
-    /// The maximum number of data points to be tested.
-    end_num_images: usize,
-    /// The increment in the number of data points from one dataset to the
-    /// next. This helps in assessing scalability and performance as the
-    /// amount of data increases.
-    step_num_images: usize,
-    /// A tuple representing the inclusive minimum and maximum values that
-    /// any single element in the data vectors can take. This is crucial for
-    /// generating test data with realistic variability.
-    value_range: (f64, f64),
-}
-
-impl BenchmarkConfig {
-    fn new(
-        dimensions_range: (usize, usize, usize),
-        num_images_range: (usize, usize, usize),
-        value_range: (f64, f64),
-    ) -> Self {
-        BenchmarkConfig {
-            start_dimensions: dimensions_range.0,
-            end_dimensions: dimensions_range.1,
-            step_dimensions: dimensions_range.2,
-            start_num_images: num_images_range.0,
-            end_num_images: num_images_range.1,
-            step_num_images: num_images_range.2,
-            value_range,
-        }
-    }
-
-    fn dataset_configurations<'a>(&'a self) -> impl Iterator<Item = (usize, usize)> + 'a {
-        (self.start_dimensions..=self.end_dimensions)
-            .step_by(self.step_dimensions)
-            .flat_map(|dimensions| {
-                (self.start_num_images..=self.end_num_images)
-                    .step_by(self.step_num_images)
-                    .map(move |num_images| (dimensions, num_images))
-            })
-    }
 }
 
 fn run_benchmark<Q: Query + 'static>() {
