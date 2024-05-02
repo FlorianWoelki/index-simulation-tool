3f291125a6efc138b1c090be7d6a1c44cf3c8067
FlorianWoelki <FlorianWoelki@gmx.de>
2024-05-02 09:26:04 +0200

    docs(benchmark): add documentation around the scalability factor

--- src/benchmark/metrics.rs
+++ src/benchmark/metrics.rs
@@ -5,6 +5,9 @@ use super::BenchmarkResult;
 /// Calculates the number of queries per second from the total time taken to execute
 /// the queries.
 ///
+/// # Arguments
+/// - `total_queries_execution_time`: The total time taken to execute the queries.
+///
 /// # Examples
 /// ```
 /// use std::time::Duration;
@@ -22,6 +25,49 @@ pub fn calculate_queries_per_second(total_queries_execution_time: Duration) -> f
     }
 }
 
+/// Calculates the scalability factor based on the performance change between two benchmarks.
+///
+/// This function computes how the queries per second (QPS) performance metric scales with
+/// changes in dataset size and dimensionality. A scalability factor greater than 1 indicates
+/// performance improvements relative to dataset growth, while a value less than 1 indicates
+/// performance degradation.
+///
+/// # Arguments
+/// - (`queries_per_second`, `dataset_size`, `dataset_dimensionality`): The performance metrics
+/// of the current benchmark.
+/// - `previous_result`: The performance metrics of the previous benchmark.
+///
+/// # Examples
+/// ```
+/// use benchmark::metrics::calculate_scalability_factor;
+/// use benchmark::BenchmarkResult;
+///
+/// let previous_result = BenchmarkResult {
+///    total_execution_time: std::time::Duration::from_secs(1),
+///    index_execution_time: std::time::Duration::from_secs(1),
+///    query_execution_time: std::time::Duration::from_secs(1),
+///    queries_per_second: 1.0,
+///    dataset_size: 1,
+///    dataset_dimensionality: 1,
+///    scalability_factor: None,
+/// };
+///
+/// let current_result = BenchmarkResult {
+///   total_execution_time: std::time::Duration::from_secs(1),
+///   index_execution_time: std::time::Duration::from_secs(1),
+///   query_execution_time: std::time::Duration::from_secs(1),
+///   queries_per_second: 2.0,
+///   dataset_size: 2,
+///   dataset_dimensionality: 2,
+///   scalability_factor: None,
+/// };
+///
+/// let scalability_factor = calculate_scalability_factor(
+///   (current_result.queries_per_second, current_result.dataset_size, current_result.dataset_dimensionality),
+///   &previous_result,
+/// );
+/// assert_eq!(scalability_factor, 2.0);
+/// ```
 pub fn calculate_scalability_factor(
     (queries_per_second, dataset_size, dataset_dimensionality): (f64, usize, usize),
     previous_result: &BenchmarkResult,
