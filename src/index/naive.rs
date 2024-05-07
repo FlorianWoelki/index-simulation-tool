23b9d085a12d3d0f1b4180f7699ee66feee88078
FlorianWoelki <FlorianWoelki@gmx.de>
2024-05-07 09:05:47 +0200

    test(index): add unit tests for naive indexing algorithm

--- src/index/naive.rs
+++ src/index/naive.rs
@@ -1,3 +1,5 @@
+use std::collections::BinaryHeap;
+
 use crate::data::HighDimVector;
 
 use super::{DistanceMetric, Index};
@@ -56,5 +58,53 @@ impl Index for NaiveIndex {
             .take(k)
             .map(|node| self.indexed_vectors[node.index].clone())
             .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_vector() {
+        let mut index = NaiveIndex::new(DistanceMetric::Euclidean);
+        let v0 = HighDimVector::new(0, vec![1.0, 2.0, 3.0]);
+
+        index.add_vector(v0.clone());
+
+        assert_eq!(index.vectors.len(), 1);
+        assert_eq!(index.vectors[0], v0);
+    }
+
+    #[test]
+    fn test_build() {
+        let mut index = NaiveIndex::new(DistanceMetric::Euclidean);
+        let v0 = HighDimVector::new(0, vec![1.0, 2.0, 3.0]);
+        let v1 = HighDimVector::new(1, vec![4.0, 5.0, 6.0]);
+
+        index.add_vector(v0.clone());
+        index.add_vector(v1.clone());
+        index.build();
+
+        assert_eq!(index.indexed_vectors.len(), 2);
+        assert_eq!(index.indexed_vectors[0], v0);
+        assert_eq!(index.indexed_vectors[1], v1);
+    }
+
+    #[test]
+    fn test_search() {
+        let mut index = NaiveIndex::new(DistanceMetric::Euclidean);
+        let v0 = HighDimVector::new(0, vec![1.0, 2.0]);
+        let v1 = HighDimVector::new(1, vec![3.0, 4.0]);
+
+        index.add_vector(v0.clone());
+        index.add_vector(v1.clone());
+        index.build();
+
+        let query_vector = HighDimVector::new(2, vec![2.0, 3.0]);
+        let results = index.search(&query_vector, 1);
+
+        assert_eq!(results.len(), 1);
+        assert_eq!(results[0], v0);
     }
 }
