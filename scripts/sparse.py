8c1297aee9b646714f961ba070664cb579564f8f
FlorianWoelki <FlorianWoelki@gmx.de>
2024-05-24 09:02:37 +0200

    feat(scripts): add querying and groundtruth check

--- scripts/sparse.py
+++ scripts/sparse.py
@@ -49,3 +49,34 @@ for i in range(0, vec_count):
     point = data[i]
     indices, values = csr_to_sparse_vector(point)
     print(values)
+    break
+
+# https://storage.googleapis.com/ann-challenge-sparse-vectors/csr/queries.dev.csr.gzip
+query_data = read_sparse_matrix("queries.dev.csr")
+query_count = query_data.shape[0]
+
+for i in range(0, query_count):
+    point = query_data[i]
+    query_vector = csr_to_sparse_vector(point)
+    print(query_vector)
+
+    # TODO: Search for nearest neighbor
+    results = []
+
+    # Checking the groundtruth
+    expected_scores = gt_scores[i]
+    expected_ids = gt_indices[i]
+    for j in range(0, len(results)):
+        result = results[j]
+        result_score = result.score
+        expected_score = float(expected_scores[j])
+
+        # if not compare_floats_percentage(result_score, expected_score, 1):
+            # print("Score mismatch: %f != %f" % (result_score, expected_score))
+
+        result_id = result.id
+        expected_id = expected_ids[j]
+        if result_id != expected_id:
+            print("ID mismatch: %d != %d" % (result_id, expected_id))
+
+    break
