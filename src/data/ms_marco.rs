c7c0f5161ea6b6a0d590f2f88308e0ae96520dd0
FlorianWoelki <FlorianWoelki@gmx.de>
2024-05-27 09:38:29 +0200

    refactor(data): remove async processing for dataset

--- src/data/ms_marco.rs
+++ src/data/ms_marco.rs
@@ -1,94 +1,86 @@
 use std::{
-    io::{Error, SeekFrom},
+    fs::File,
+    io::{BufReader, Error, Seek, SeekFrom},
     path::PathBuf,
 };
 
-use byteorder::{ByteOrder, LittleEndian};
+use byteorder::{LittleEndian, ReadBytesExt};
 use sprs::CsMat;
-use tokio::{
-    fs::File,
-    io::{AsyncReadExt, AsyncSeekExt},
-    task::{self, JoinSet},
-};
 
-async fn read_sparse_matrix_fields(
-    file_path: &PathBuf,
-) -> Result<(Vec<f32>, Vec<usize>, Vec<usize>, usize), Error> {
-    let mut file = File::open(file_path).await?;
+// Dataset details:
+// https://github.com/harsha-simhadri/big-ann-benchmarks/blob/1fafd825989fbfca9c7896384ba86a5a359bf388/dataset_preparation/sparse_dataset.md?plain=1#L34
 
-    let mut sizes = [0u8; 24];
-    file.read_exact(&mut sizes).await?;
-    let nrow = LittleEndian::read_i64(&sizes[0..8]);
-    let ncol = LittleEndian::read_i64(&sizes[8..16]);
-    let nnz = LittleEndian::read_i64(&sizes[16..24]);
+fn read_sparse_matrix_fields(fname: &PathBuf) -> (Vec<f32>, Vec<usize>, Vec<usize>, usize) {
+    let mut reader = BufReader::new(File::open(fname).expect("Unable to open file"));
+
+    let mut sizes = [0i64; 3];
+    reader
+        .read_i64_into::<LittleEndian>(&mut sizes)
+        .expect("Unable to read size");
+    let (nrow, ncol, nnz) = (sizes[0] as usize, sizes[1] as usize, sizes[2] as usize);
 
     let mut indptr = vec![0usize; (nrow + 1) as usize];
-    let mut buf = vec![0u8; indptr.len() * 8];
-    file.read_exact(&mut buf).await?;
-    for i in 0..indptr.len() {
-        indptr[i] = LittleEndian::read_i64(&buf[i * 8..(i + 1) * 8]) as usize;
+    for i in &mut indptr {
+        *i = reader
+            .read_i64::<LittleEndian>()
+            .expect("Unable to read indptr") as usize;
     }
-    assert_eq!(nnz, *indptr.last().unwrap() as i64);
+    assert_eq!(nnz, *indptr.last().unwrap());
 
-    let mut indices = vec![0usize; nnz as usize];
-    let mut buf = vec![0u8; indices.len() * 4];
-    file.read_exact(&mut buf).await?;
-    for i in 0..indices.len() {
-        indices[i] = LittleEndian::read_i32(&buf[i * 4..(i + 1) * 4]) as usize;
+    let mut indices = vec![0usize; nnz];
+    for i in &mut indices {
+        *i = reader
+            .read_i32::<LittleEndian>()
+            .expect("Unable to read indices") as usize;
     }
 
     let mut data = vec![0f32; nnz as usize];
-    let mut buf = vec![0u8; data.len() * 4];
-    file.read_exact(&mut buf).await?;
-    LittleEndian::read_f32_into(&buf, &mut data);
+    reader
+        .read_f32_into::<LittleEndian>(&mut data)
+        .expect("Unable to read data");
 
-    Ok((data, indices, indptr, ncol as usize))
+    (data, indices, indptr, ncol as usize)
 }
 
-async fn read_sparse_matrix(file_path: &PathBuf) -> Result<CsMat<f32>, Error> {
-    let (data, indices, indptr, ncol) = read_sparse_matrix_fields(file_path).await?;
-    Ok(CsMat::new((indptr.len() - 1, ncol), indptr, indices, data))
+fn read_sparse_matrix(fname: &PathBuf) -> CsMat<f32> {
+    let (data, indices, indptr, ncol) = read_sparse_matrix_fields(fname);
+    CsMat::new((indptr.len() - 1, ncol), indptr, indices, data)
 }
 
-async fn knn_result_read(file_path: &PathBuf) -> Result<(Vec<Vec<i32>>, Vec<Vec<f32>>), Error> {
-    // TODO: Maybe change that to match the other data types.
-    let mut file = File::open(file_path).await?;
+fn knn_result_read(fname: &PathBuf) -> (Vec<Vec<i32>>, Vec<Vec<f32>>) {
+    let mut file = File::open(fname).expect("Unable to open file");
 
-    let mut sizes = [0u8; 8];
-    file.read_exact(&mut sizes).await?;
-    let n = LittleEndian::read_u32(&sizes[0..4]) as usize;
-    let d = LittleEndian::read_u32(&sizes[4..8]) as usize;
+    let n = file.read_u32::<LittleEndian>().expect("Unable to read n") as usize;
+    let d = file.read_u32::<LittleEndian>().expect("Unable to read d") as usize;
 
     let expected_size = 8 + n * d * (4 + 4);
-    assert_eq!(expected_size as u64, file.metadata().await?.len());
+    assert_eq!(expected_size as u64, file.metadata().unwrap().len());
 
-    file.seek(SeekFrom::Start(8)).await?;
+    file.seek(SeekFrom::Start(8)).unwrap();
 
     let mut ids = vec![vec![0i32; d]; n];
-    let mut buf = vec![0u8; n * d * 4];
-    file.read_exact(&mut buf).await?;
-    for (i, row) in ids.iter_mut().enumerate() {
-        for (j, id) in row.iter_mut().enumerate() {
-            *id = LittleEndian::read_i32(&buf[(i * d + j) * 4..]);
+    for row in &mut ids {
+        for id in row {
+            *id = file.read_i32::<LittleEndian>().expect("Unable to read id");
         }
     }
 
     let mut scores = vec![vec![0f32; d]; n];
-    let mut buf = vec![0u8; n * d * 4];
-    file.read_exact(&mut buf).await?;
-    for (i, row) in scores.iter_mut().enumerate() {
-        for (j, score) in row.iter_mut().enumerate() {
-            *score = LittleEndian::read_f32(&buf[(i * d + j) * 4..]);
+    for row in &mut scores {
+        for score in row {
+            *score = file
+                .read_f32::<LittleEndian>()
+                .expect("Unable to read score");
         }
     }
 
-    Ok((ids, scores))
+    (ids, scores)
 }
 
 type SparseVectorData = Vec<(Vec<usize>, Vec<f32>)>; // indices, values
 
 // groundtruth, vectors, query_vectors
-pub async fn load_msmarco_dataset() -> Result<
+pub fn load_msmarco_dataset() -> Result<
     (
         (Vec<Vec<i32>>, Vec<Vec<f32>>),
         SparseVectorData,
@@ -103,37 +95,23 @@ pub async fn load_msmarco_dataset() -> Result<
     let vectors_file = msmarco_dir.join("base_small.csr");
     let query_file = msmarco_dir.join("queries.dev.csr");
 
-    let gt_future = task::spawn(async move { knn_result_read(&gt_file).await });
-    let vectors_future = task::spawn(async move { read_sparse_matrix(&vectors_file).await });
-    let queries_future = task::spawn(async move { read_sparse_matrix(&query_file).await });
+    let groundtruth = knn_result_read(&gt_file);
+    let vector_data = read_sparse_matrix(&vectors_file);
+    let query_data = read_sparse_matrix(&query_file);
 
-    let groundtruth = gt_future.await??;
-    let data = vectors_future.await??;
-    let query_data = queries_future.await??;
-
-    let mut join_set = JoinSet::new();
-    for i in 0..data.rows() {
-        let indices = data.outer_view(i).unwrap().indices().to_vec();
-        let values = data.outer_view(i).unwrap().data().to_vec();
-        join_set.spawn(async move { (indices, values) });
+    let mut result_vectors = vec![];
+    for i in 0..vector_data.rows() {
+        let indices = vector_data.outer_view(i).unwrap().indices().to_vec();
+        let values = vector_data.outer_view(i).unwrap().data().to_vec();
+        result_vectors.push((indices, values));
     }
 
-    let mut vectors: SparseVectorData = Vec::with_capacity(data.rows());
-    while let Some(res) = join_set.join_next().await {
-        vectors.push(res?);
-    }
-
-    let mut join_set = JoinSet::new();
+    let mut query_vectors = vec![];
     for i in 0..query_data.rows() {
         let indices = query_data.outer_view(i).unwrap().indices().to_vec();
         let values = query_data.outer_view(i).unwrap().data().to_vec();
-        join_set.spawn(async move { (indices, values) });
+        query_vectors.push((indices, values));
     }
 
-    let mut query_vectors: SparseVectorData = Vec::with_capacity(query_data.rows());
-    while let Some(res) = join_set.join_next().await {
-        query_vectors.push(res?);
-    }
-
-    Ok((groundtruth, vectors, query_vectors))
+    Ok((groundtruth, result_vectors, query_vectors))
 }
