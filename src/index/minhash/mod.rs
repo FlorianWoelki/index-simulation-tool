02da18518555ce829985854d81fce4e666383ccc
FlorianWoelki <FlorianWoelki@gmx.de>
2024-06-20 19:41:24 +0200

    refactor(index): minor changes to MinHash LSH algorithm

--- src/index/minhash/mod.rs
+++ src/index/minhash/mod.rs
@@ -1,189 +1,147 @@
-use std::{
-    collections::{HashMap, HashSet},
-    hash::{DefaultHasher, Hash, Hasher},
-};
+use std::hash::{DefaultHasher, Hash, Hasher};
 
 use ordered_float::OrderedFloat;
-use rand::{rngs::StdRng, Rng, SeedableRng};
 
-use super::{QueryResult, SparseVector};
+use crate::{
+    data::{QueryResult, SparseVector},
+    data_structures::min_heap::MinHeap,
+};
 
-pub struct MinHash {
-    num_permutations: usize,
-    hash_functions: Vec<Box<dyn Fn(usize) -> u64>>,
-}
-
-impl MinHash {
-    fn new_with_rng(num_permutations: usize, seed: u64) -> Self {
-        assert!(
-            num_permutations > 0,
-            "Number of hashes must be greater than 0"
-        );
-
-        let mut rng = StdRng::seed_from_u64(seed);
-        let mut hash_functions = Vec::new();
-
-        for _ in 0..num_permutations {
-            let a = rng.gen::<u64>();
-            let b = rng.gen::<u64>();
-            hash_functions.push(Box::new(move |x: usize| -> u64 {
-                let mut hasher = DefaultHasher::new();
-                (a.wrapping_mul(x as u64).wrapping_add(b)).hash(&mut hasher);
-                hasher.finish()
-            }) as Box<dyn Fn(usize) -> u64>);
-        }
-        Self {
-            num_permutations,
-            hash_functions,
-        }
-    }
-
-    pub fn compute_signature(&self, vector: &SparseVector) -> Vec<u64> {
-        let mut signature = vec![u64::MAX; self.num_permutations];
-
-        for &index in &vector.indices {
-            for (i, hash_function) in self.hash_functions.iter().enumerate() {
-                let hash_value = hash_function(index);
-                if hash_value < signature[i] {
-                    signature[i] = hash_value;
-                }
-            }
-        }
-
-        signature
-    }
-
-    pub fn jaccard_similarity(sig1: &[u64], sig2: &[u64]) -> f32 {
-        let num_equal = sig1
-            .iter()
-            .zip(sig2.iter())
-            .filter(|&(a, b)| a == b)
-            .count();
-        num_equal as f32 / sig1.len() as f32
-    }
-}
-
-pub struct LSHFunction {
-    a: u64,
-    b: u64,
+pub struct LSHIndex {
     num_buckets: usize,
-}
-
-impl LSHFunction {
-    fn new_with_rng(num_buckets: usize, seed: u64) -> Self {
-        assert!(num_buckets > 0, "Number of buckets must be greater than 0");
-
-        let mut rng = StdRng::seed_from_u64(seed);
-        let a = rng.gen::<u64>();
-        let b = rng.gen::<u64>();
-        Self { a, b, num_buckets }
-    }
-
-    fn hash(&self, signature: &[u64]) -> usize {
-        let mut hash_value = 0u64;
-        for &value in signature {
-            hash_value = hash_value
-                .wrapping_mul(self.a)
-                .wrapping_add(value)
-                .wrapping_add(self.b);
-        }
-        (hash_value as usize) % self.num_buckets
-    }
-}
-
-pub struct MinHashIndex {
-    pub minhash: MinHash,
-    lsh_functions: Vec<LSHFunction>,
+    num_hash_functions: usize,
+    buckets: Vec<Vec<(usize, SparseVector)>>,
     vectors: Vec<SparseVector>,
-    buckets: HashMap<usize, Vec<(SparseVector, usize, Vec<u64>)>>,
 }
 
-impl MinHashIndex {
-    pub fn new_with_rng(
-        num_permutations: usize,
-        num_lsh_functions: usize,
-        num_buckets: usize,
-        seed: u64,
-    ) -> Self {
+impl LSHIndex {
+    pub fn new(num_buckets: usize, num_hash_functions: usize) -> Self {
         assert!(
-            num_permutations > 0,
-            "Number of permutations must be greater than zero."
+            num_hash_functions >= 2,
+            "num_hash_functions must be at least 2"
         );
-        assert!(
-            num_lsh_functions > 0,
-            "Number of LSH functions must be greater than zero."
-        );
-        assert!(
-            num_buckets > 0,
-            "Number of buckets must be greater than zero."
-        );
-
-        let mut rng = StdRng::seed_from_u64(seed);
-        let minhash = MinHash::new_with_rng(num_permutations, rng.gen::<u64>());
-        let mut lsh_functions = Vec::with_capacity(num_lsh_functions);
-        for _ in 0..num_lsh_functions {
-            lsh_functions.push(LSHFunction::new_with_rng(num_buckets, rng.gen::<u64>()));
-        }
-        Self {
-            minhash,
-            lsh_functions,
-            buckets: HashMap::new(),
+        LSHIndex {
+            num_buckets,
+            num_hash_functions,
+            buckets: vec![Vec::new(); num_buckets],
             vectors: Vec::new(),
         }
     }
 
-    pub fn add_vector(&mut self, vector: &SparseVector) {
-        let signature = self.minhash.compute_signature(&vector);
-        for lsh_function in &self.lsh_functions {
-            let bucket_key = lsh_function.hash(&signature);
-            self.buckets
-                .entry(bucket_key)
-                .or_insert_with(Vec::new)
-                .push((vector.clone(), self.vectors.len(), signature.clone()));
-        }
+    /// Based on the MurmurHash3 algorithm.
+    fn hash_bucket(&self, hash: u64) -> usize {
+        let a: u64 = 0x9e3779b97f4a7c15;
+        let b: u64 = 0xbb67ae8584caa73b;
+        let c: u64 = 0x637c835768936735;
+        let d: u64 = 0xf87694f3329e33d1;
+        let hash = hash
+            .wrapping_mul(a)
+            .rotate_left(47)
+            .wrapping_mul(b)
+            .rotate_right(43)
+            .wrapping_mul(c)
+            .wrapping_add(d);
+        (hash as usize) % self.num_buckets
+    }
 
+    pub fn add_vector(&mut self, vector: &SparseVector) {
+        for i in 0..self.num_hash_functions {
+            let hash = self.minhash(vector, i);
+            let bucket_index = self.hash_bucket(hash);
+            self.buckets[bucket_index].push((self.vectors.len(), vector.clone()));
+        }
         self.vectors.push(vector.clone());
     }
 
-    pub fn search(&self, query_vector: &SparseVector, k: usize) -> Vec<QueryResult> {
-        let query_signature = self.minhash.compute_signature(query_vector);
-        let mut candidates = HashSet::new();
+    pub fn build(&mut self) {
+        for bucket in &mut self.buckets {
+            bucket.sort_by(|a, b| a.1.values.partial_cmp(&b.1.values).unwrap());
+        }
+    }
 
-        for lsh_function in &self.lsh_functions {
-            let bucket_key = lsh_function.hash(&query_signature);
-            if let Some(bucket) = self.buckets.get(&bucket_key) {
-                for (_, _, signature) in bucket {
-                    candidates.insert(signature);
+    pub fn search(&self, query_vector: &SparseVector, k: usize) -> Vec<QueryResult> {
+        let mut results: Vec<(f32, usize, SparseVector, usize)> = Vec::new();
+
+        for i in 0..self.num_hash_functions {
+            let query_hash = self.minhash(query_vector, i);
+            let bucket_index = self.hash_bucket(query_hash);
+            let bucket = &self.buckets[bucket_index];
+
+            for (index, vector) in bucket.iter() {
+                let similarity = query_vector.cosine_similarity(&vector);
+                let mut found = false;
+
+                for (existing_similarity, _, existing_vector, bucket_count) in &mut results {
+                    if *existing_vector == *vector {
+                        *existing_similarity += similarity;
+                        *bucket_count += 1;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if !found {
+                    results.push((similarity, *index, vector.clone(), 1));
                 }
             }
         }
 
-        let mut results: Vec<(f32, usize, &SparseVector)> = candidates
+        for (similarity, _, _, bucket_count) in &mut results {
+            *similarity /= *bucket_count as f32;
+        }
+
+        let mut heap: MinHeap<QueryResult> = MinHeap::new();
+        for (score, index, _, _) in results.iter() {
+            if heap.len() < k || *score > heap.peek().unwrap().score.into_inner() {
+                heap.push(
+                    QueryResult {
+                        index: *index,
+                        score: OrderedFloat(*score),
+                    },
+                    OrderedFloat(*score),
+                );
+                if heap.len() > k {
+                    heap.pop();
+                }
+            }
+        }
+
+        heap.into_sorted_vec()
             .iter()
-            .filter_map(|signature| {
-                self.buckets.values().find_map(|bucket| {
-                    bucket
-                        .iter()
-                        .find(|(_, _, sig)| sig == *signature)
-                        .map(|(vector, index, _)| {
-                            let similarity =
-                                MinHash::jaccard_similarity(&query_signature, signature);
-                            (similarity, *index, vector)
-                        })
-                })
-            })
-            .collect();
-
-        results.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
-
-        results
-            .into_iter()
-            .take(k)
-            .map(|(similarity, index, _)| QueryResult {
-                index,
-                score: OrderedFloat(similarity),
+            .map(|query_result| QueryResult {
+                index: query_result.index,
+                score: OrderedFloat(query_result.score.into_inner()),
             })
             .collect()
+    }
+
+    fn minhash(&self, vector: &SparseVector, hash_function_index: usize) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        let mut min_hash: u64 = u64::MAX;
+
+        for (&index, &value) in vector.indices.iter().zip(vector.values.iter()) {
+            let mut combined_hash = hash_function_index as u64;
+            index.hash(&mut hasher);
+            combined_hash = combined_hash.wrapping_mul(hasher.finish());
+            value.hash(&mut hasher);
+            combined_hash = combined_hash.wrapping_mul(hasher.finish());
+
+            min_hash = min_hash.min(combined_hash);
+            hasher = DefaultHasher::new();
+            // let mut element_hash = HashSet::new();
+            // element_hash.insert(index);
+            // element_hash.insert(value.to_bits() as usize);
+
+            // for item in element_hash {
+            //     item.hash(&mut hasher);
+            // }
+
+            // let hash = hasher.finish() ^ (hash_function_index as u64);
+            // min_hash = min_hash.min(hash);
+            // hasher = DefaultHasher::new();
+        }
+
+        min_hash
     }
 }
 
@@ -192,86 +150,73 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_lsh() {
-        let vector1 = SparseVector {
-            indices: vec![1, 2, 3],
-            values: vec![OrderedFloat(0.5), OrderedFloat(0.3), OrderedFloat(0.2)],
-        };
-        let vector2 = SparseVector {
-            indices: vec![1, 2, 4],
-            values: vec![OrderedFloat(0.4), OrderedFloat(0.4), OrderedFloat(0.2)],
-        };
-        let vector3 = SparseVector {
-            indices: vec![1, 3, 4],
-            values: vec![OrderedFloat(0.6), OrderedFloat(0.1), OrderedFloat(0.3)],
-        };
-
-        let mut index = MinHashIndex::new_with_rng(100, 10, 50, 42);
-
-        index.add_vector(&vector1);
-        index.add_vector(&vector2);
-        index.add_vector(&vector3);
-
-        let query_vector = SparseVector {
-            indices: vec![1, 2, 3],
-            values: vec![OrderedFloat(0.5), OrderedFloat(0.3), OrderedFloat(0.2)],
-        };
-
-        let results = index.search(&query_vector, 2);
-
-        for result in results {
-            println!("{:?}", result);
-        }
-
-        assert!(true);
-    }
-
-    #[test]
-    fn test_lsh_complex() {
-        let vectors = vec![
+    fn test_lsh_index_with_min_hash_simple() {
+        let data = vec![
             SparseVector {
-                indices: vec![1, 2, 3],
-                values: vec![OrderedFloat(0.5), OrderedFloat(0.3), OrderedFloat(0.2)],
+                indices: vec![0, 2],
+                values: vec![OrderedFloat(1.0), OrderedFloat(2.0)],
             },
             SparseVector {
-                indices: vec![1, 2, 4],
-                values: vec![OrderedFloat(0.4), OrderedFloat(0.4), OrderedFloat(0.2)],
+                indices: vec![1, 3],
+                values: vec![OrderedFloat(3.0), OrderedFloat(4.0)],
             },
             SparseVector {
-                indices: vec![1, 3, 4],
-                values: vec![OrderedFloat(0.6), OrderedFloat(0.1), OrderedFloat(0.3)],
+                indices: vec![0, 2],
+                values: vec![OrderedFloat(5.0), OrderedFloat(6.0)],
             },
             SparseVector {
-                indices: vec![2, 3, 5],
-                values: vec![OrderedFloat(0.3), OrderedFloat(0.3), OrderedFloat(0.4)],
+                indices: vec![1, 3],
+                values: vec![OrderedFloat(7.0), OrderedFloat(8.0)],
             },
             SparseVector {
-                indices: vec![1, 2, 5],
-                values: vec![OrderedFloat(0.2), OrderedFloat(0.5), OrderedFloat(0.3)],
-            },
-            SparseVector {
-                indices: vec![3, 4, 5],
-                values: vec![OrderedFloat(0.3), OrderedFloat(0.3), OrderedFloat(0.4)],
+                indices: vec![0, 2],
+                values: vec![OrderedFloat(9.0), OrderedFloat(10.0)],
             },
         ];
 
-        let mut index = MinHashIndex::new_with_rng(100, 10, 50, 42);
+        let mut index = LSHIndex::new(4, 4);
+        for vector in &data {
+            index.add_vector(vector);
+        }
+        index.build();
+
+        let query = SparseVector {
+            indices: vec![0, 2],
+            values: vec![OrderedFloat(6.0), OrderedFloat(7.0)],
+        };
+        let neighbors = index.search(&query, 2);
+        println!("Nearest neighbors: {:?}", neighbors);
+
+        assert!(false);
+    }
+
+    #[test]
+    fn test_lsh_index_with_min_hash_complex() {
+        let mut index = LSHIndex::new(10, 4);
+
+        let mut vectors = vec![];
+        for i in 0..100 {
+            vectors.push(SparseVector {
+                indices: vec![i % 10, (i / 10) % 10],
+                values: vec![OrderedFloat((i % 10) as f32), OrderedFloat((i / 10) as f32)],
+            });
+        }
 
         for vector in &vectors {
             index.add_vector(vector);
         }
 
+        index.build();
+
         let query_vector = SparseVector {
-            indices: vec![1, 2, 3],
-            values: vec![OrderedFloat(0.5), OrderedFloat(0.3), OrderedFloat(0.2)],
+            indices: vec![5, 9],
+            values: vec![OrderedFloat(5.0), OrderedFloat(9.0)],
         };
-
         let results = index.search(&query_vector, 10);
+        println!("Results for search on query vector: {:?}", results);
+        println!("Top Search: {:?}", vectors[results[0].index]);
+        println!("Groundtruth: {:?}", query_vector);
 
-        for result in results {
-            println!("{:?}", result);
-        }
-
-        assert!(true);
+        assert!(false);
     }
 }
