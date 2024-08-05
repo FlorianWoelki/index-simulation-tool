e449703ae2011217a68bbfdc8f0b88e752d6b0d0
FlorianWoelki <FlorianWoelki@gmx.de>
2024-08-05 09:11:56 +0200

    test: add extra unit tests

--- src/benchmark/logger.rs
+++ src/benchmark/logger.rs
@@ -57,3 +57,55 @@ impl BenchmarkLogger {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{fs, io::Read, time::Duration};
+
+    #[test]
+    fn test_add_record() {
+        let mut logger = BenchmarkLogger::new();
+        let result = BenchmarkResult {
+            total_execution_time: Duration::new(10, 0),
+            index_execution_time: Duration::new(2, 0),
+            query_execution_time: Duration::new(1, 0),
+            queries_per_second: 100.0,
+            dataset_size: 1000,
+            dataset_dimensionality: 128,
+            scalability_factor: Some(1.5),
+        };
+
+        logger.add_record(&result);
+        assert_eq!(logger.records.len(), 1);
+        assert_eq!(logger.records[0].total_execution_time, Duration::new(10, 0));
+    }
+
+    #[test]
+    fn test_write_to_csv() {
+        let mut logger = BenchmarkLogger::new();
+        let result = BenchmarkResult {
+            total_execution_time: Duration::new(10, 0),
+            index_execution_time: Duration::new(2, 0),
+            query_execution_time: Duration::new(1, 0),
+            queries_per_second: 100.0,
+            dataset_size: 1000,
+            dataset_dimensionality: 128,
+            scalability_factor: Some(1.5),
+        };
+
+        logger.add_record(&result);
+
+        let file_path = "test_benchmark.csv";
+        logger.write_to_csv(file_path).unwrap();
+
+        let mut file = File::open(file_path).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+
+        assert!(contents.contains("total_execution_time,index_execution_time,query_execution_time,queries_per_second,dataset_size,dataset_dimensionality,scalability_factor"));
+        assert!(contents.contains("10.0,2.0,1.0,100.0,1000,128,1.5"));
+
+        fs::remove_file(file_path).unwrap();
+    }
+}
