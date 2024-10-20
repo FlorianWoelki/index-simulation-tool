c112f2d0a17539cbd1a2d862b24a5f494030b7f9
FlorianWoelki <FlorianWoelki@gmx.de>
2024-10-20 10:14:05 +0200

    refactor: adjust splade script to generate groundtruth and query vectors from text

--- scripts/splade.py
+++ scripts/splade.py
@@ -7,7 +7,16 @@ import torch
 import numpy as np
 
 model_id = 'naver/splade-cocondenser-ensembledistil'
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForMaskedLM.from_pretrained(model_id)
+
 dataset_name = 'SandipPalit/Movie_Dataset'
+dataset = load_dataset(dataset_name)
+training_dataset = dataset['train']
+
+def generate_related_query(text, num_words=5):
+    words = text.split()
+    return " ".join(np.random.choice(words, min(num_words, len(words)), replace=False))
 
 def create_splade(text):
     tokens = tokenizer(text, return_tensors='pt')
@@ -17,42 +26,38 @@ def create_splade(text):
             1 + torch.relu(output.logits)
         ) * tokens.attention_mask.unsqueeze(-1),
     dim=1)[0].squeeze()
-
     return vec
 
 def clean_text(text):
     return re.sub(r'[\r\n\t]+', ' ', text)
 
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForMaskedLM.from_pretrained(model_id)
-
-dataset = load_dataset(dataset_name)
-training_dataset = dataset['train']
-
 texts = []
-vectors = []
-
-for i, example in enumerate(tqdm(training_dataset, desc="Processing")):
+for example in tqdm(training_dataset, desc="Processing"):
     text = clean_text(example['Overview'])
     texts.append(text)
-    vector = create_splade(text)
-    vectors.append(vector.tolist())
-    if i == 11: # temporary
+    if len(texts) == 11: # temporary
         break
 
-df = pd.DataFrame({
-    'text': texts,
-    'splade_vector': vectors
-})
-query = "Space exploration"
-df.at[0, 'text'] = query
-df.at[0, 'splade_vector'] = create_splade(query).tolist()
+# Generate sparse vectors.
+sparse_vectors = []
+for text in tqdm(texts, desc="Generating sparse vectors"):
+    sparse_vectors.append(create_splade(text))
 
-d = {}
-for num in np.array(df['splade_vector'].iloc[4]):
-    if num in d:
-        d[num] += 1
-    else:
-        d[num] = 1
+num_queries = 2
+queries = []
+query_vectors = []
+groundtruth = []
+for _ in range(num_queries):
+    random_doc = np.random.choice(texts)
 
-print(d)
+    query = generate_related_query(random_doc)
+    query_vector = create_splade(query)
+    queries.append(query)
+    query_vectors.append(query_vector)
+
+    groundtruth.append(random_doc)
+
+print(f"Generated {len(texts)} document vectors, {num_queries} query vectors.")
+print(f"Sample query: {queries[0]}")
+print(f"Sample groundtruth: {groundtruth[0]}")
+print("Dataset saved to 'splade_sparse_vectors_dataset.csv'")
