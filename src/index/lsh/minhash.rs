0f653363fe59f32c5633ac998f73107497f58683
FlorianWoelki <FlorianWoelki@gmx.de>
2025-02-26 10:32:47 +0100

    refactor: clean up minhash computation

--- src/index/lsh/minhash.rs
+++ src/index/lsh/minhash.rs
@@ -13,17 +13,6 @@ pub(super) fn minhash(vector: &SparseVector, hash_function_index: usize) -> u64 
         let combined_hash = hasher.finish();
         min_hash = min_hash.min(combined_hash);
         hasher = DefaultHasher::new();
-        // let mut element_hash = HashSet::new();
-        // element_hash.insert(index);
-        // element_hash.insert(value.to_bits() as usize);
-
-        // for item in element_hash {
-        //     item.hash(&mut hasher);
-        // }
-
-        // let hash = hasher.finish() ^ (hash_function_index as u64);
-        // min_hash = min_hash.min(hash);
-        // hasher = DefaultHasher::new();
     }
 
     min_hash
